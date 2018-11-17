4a9053cbab95e9a9bbed4df782452f8cba6ab89c
taaaka <Rw.taaaka@gmail.com>
2018-11-18 06:51:15 +0900

    [modify] WIP: post todo

--- src/functions/main/post_todo/index.ts
+++ src/functions/main/post_todo/index.ts
@@ -1,21 +1,54 @@
 import {APIGatewayEvent, Context, Callback} from 'aws-lambda';
 
 import localConfigure from '../../localConfigure';
-import {Tables} from '../../../types/todo_api';
+import {Tables, StatusCode, ContentType} from '../../../types/todo_api';
 import {insertTodo} from '../../../db/todo';
 
 localConfigure();
 
 const handler = async (event: APIGatewayEvent, context: Context, callback: Callback) => {
+  const id = `${new Date().getTime()}`;
+  const body = JSON.parse(event.body);
+
   const insertRecord: Tables.TodoRecord = {
-    id: 'TODO0003',
-    title: 'hoge',
-    description: 'todo description',
-    done: 0,
-    priority: 3
+    id,
+    title: body.title,
+    done: 0
   };
 
-  await insertTodo(insertRecord);
+  if (body.description) {
+    insertRecord.description = body.description;
+  }
+  if (body.priority) {
+    insertRecord.priority = body.priority;
+  }
+
+  let ret = true;
+  try {
+    await insertTodo(insertRecord);
+  } catch (error) {
+    ret = false;
+  }
+
+  if (ret) {
+    callback(null, {
+      statusCode: StatusCode.OK,
+      headers: {
+        'Content-Type': ContentType.APPLICATION_JSON
+      },
+      body: JSON.stringify(insertRecord)
+    });
+  } else {
+    callback(null, {
+      statusCode: StatusCode.INTERNAL_SERVER_ERROR,
+      headers: {
+        'Content-Type': ContentType.APPLICATION_JSON
+      },
+      body: JSON.stringify({
+        message: 'Error has occurred!!'
+      })
+    });
+  }
 };
 
 export {handler};
